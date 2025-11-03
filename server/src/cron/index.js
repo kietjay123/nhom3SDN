6e79473d7abc3081862f30a43396925d70d0082d
kietjay123 <kietjay1234@gmail.com>
2025-11-04 01:56:53 +0700

    damn

--- server/src/cron/index.js
+++
@@ -1,9 +0,0 @@
-const startInventoryJob = require('./inventoryJob');
-const { startReminderEmailJob } = require('./reminderEmailJob');
-
-function startAllCrons() {
-  startInventoryJob();
-  startReminderEmailJob();
-}
-
-module.exports = startAllCrons;
